1bda8a6461d0c41422b8cf17d772da7274091495
xaqan05 <xaqan2005@gmail.com>
2024-10-11 11:55:35 +0400

    version

---
+++ Task(11.10)/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_11._10_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3 };
+            InsertArray(ref numbers, 4, 5, 6);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+
+        public static void InsertArray(ref int[] array, params int[] values)
+        {
+            int newSize = array.Length + values.Length;
+
+            int[] newArray = new int[newSize];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                newArray[array.Length + i] = values[i];
+            }
+
+            array = newArray;
+        }
+
+    }
+}
